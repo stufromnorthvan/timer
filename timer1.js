ec5fae1f278c684d16edacb5d59c028d920ab0f1
stufromnorthvan <stumitchell@irsss.ca>
2022-09-27 16:38:17 -0700

    adding conditionals to timer, forgot first time around

--- timer1.js
+++ timer1.js
@@ -1,7 +1,9 @@
-const args = process.argv.slice(2).sort(function(a, b){return a - b})
-
+const args = process.argv.slice(2).sort(function(a, b){return a - b}).filter(word => word > 0 && word !== NaN)
 
 let timer = function(args) {
+  if (args.length === 0) {
+    return;
+  }
   for (let time of args) {
     setTimeout(() => {
       process.stdout.write('\x07');}
